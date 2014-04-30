4a3914a73db638d7ab263414f9958d8a693bd307
akherr <akherr@vt.edu>
2014-04-30 17:29:05 -0400

    random php files push

---
+++ crs-cat-entry.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <?php
+    ini_set('display_errors','1');
+    ini_set('display_startup_errors','1');
+  ?>
+  <head>
+  <title> Course Catalogue System </title>
+  <script type = "text/javascript">
+	//this function is to be called when the submit button is clicked. it will return false if the validation finds an error
+  	function isValid()
+  	{
+  		var valid = true;
+
+  		//these two variables hold the regular expressions for the input validation
+  		var namePattern = "^[A-Z|a-z]+-*";
+  		var courseNumPattern = "^[1-5][0-9][0-9]$";
+
+  		regexpname = new RegExp(namePattern);
+  		regexpcoursenum = new RegExp(courseNumPattern);
+
+
+		//these variables hold references to the multiple checkboxes within the form
+		var chCSAT = document.myform.csat.checked;
+		var chISAS = document.myform.isas.checked;
+		var chCS = document.myform.cs.checked;
+		var chDB = document.myform.db.checked;
+  		var chSE = document.myform.se.checked;
+  		var chN =  document.myform.n.checked;
+		var chIS = document.myform.is.checked;
+  		var chWD = document.myform.wd.checked;
+
+		//these variables hold references to the multiple textboxes within the form
+  		var first = document.getElementById('first');
+  		var last = document.getElementById('last');
+		var coursenum = document.getElementById('coursenum');
+		var coursetitle = document.getElementById('coursetitle');
+		var coursedesc = document.getElementById('coursedesc');
+		var dept = document.getElementById('dept');
+
+		//these if statements check for valid format within the first name, last name, course number, course title, and course description
+		if(!(first.value.match(namePattern)))
+		{
+			alert("first name has incorrect format.");
+			valid = false;
+		}
+		if(!(last.value.match(namePattern)))
+		{
+			alert("last name has incorrect format.");
+			valid = false;
+		}
+		if(dept.value == "")
+		{
+			alert("you must select a department");
+			valid = false;
+		}
+		if(!(coursenum.value.match(courseNumPattern)))
+		{
+			alert("course number has incorrect format.");
+			valid = false;
+		}
+		if(!(coursetitle.value.match(namePattern)))
+		{
+			alert("course title has incorrect format.");
+			valid = false;
+		}
+		if(coursedesc.value.length > 400)
+		{
+			alert("course description has too many characters");
+			valid = false;
+		}
+
+		//This if statement checks for validation within the checkboxes for the major concentrations
+		if(chCSAT == true)
+		{
+			if(chISAS == true)
+			{
+				if(!((chCS == true || chDB == true || chSE == true || chN == true) && (chIS == true || chWD == true)))
+				{
+					alert("not enough concentrations selected.");
+					valid = false;
+				}
+			}
+			else
+			{
+				if(!(chCS == true || chDB == true || chSE == true || chN == true))
+				{
+					alert("you must select a concentration.");
+					valid = false;
+				}
+			}
+		}
+		else if(chISAS == true)
+		{
+			if(chISAS == true)
+			{
+				if(!(chIS == true || chWD == true))
+				{
+					alert("you must select a concentration.");
+					valid = false;
+				}
+			}
+		}
+		else
+		{
+			alert("you must select a major");
+			valid = false;
+		}
+
+
+
+  		return valid;
+  	}
+
+	//this function is to be called when the 'CSAT' checkbox is clicked. It will either disable or enable the associated
+  	     //concentration checkboxes depending on if the 'CSAT' checkbox is checked
+  	function checkedCS()
+  	{
+  		var chCSAT = document.myform.csat.checked;
+  		if(chCSAT == true)
+  		{
+			document.myform.cs.disabled = false;
+			document.myform.db.disabled = false;
+			document.myform.se.disabled = false;
+			document.myform.n.disabled = false;
+		}
+		else
+		{
+			document.myform.cs.disabled = true;
+			document.myform.cs.checked = false;
+			document.myform.db.disabled = true;
+			document.myform.db.checked = false;
+			document.myform.se.disabled = true;
+			document.myform.se.checked = false;
+			document.myform.n.disabled = true;
+			document.myform.n.checked = false;
+		}
+	}
+
+	//this function is to be called when the 'ISAS' checkbox is clicked. It will either disable or enable the associated
+  	     //concentration checkboxes depending on if the 'ISAS' checkbox is checked
+	function checkedIS()
+	{
+		var chISAS = document.myform.isas.checked;
+		if(chISAS == true)
+		{
+			document.myform.is.disabled = false;
+			document.myform.wd.disabled = false;
+		}
+		else
+		{
+			document.myform.is.disabled = true;
+			document.myform.is.checked = false;
+			document.myform.wd.disabled = true;
+			document.myform.wd.checked = false;
+		}
+	}
+  </script>
+  <style type = "text/css">
+  	p.head { color: black;
+    	margin-top: 50px;
+        margin-left: 150px;
+        margin-right: 150px;
+        border-style: solid;
+        border-color: black;
+        border-width: 10px;
+        padding: 30px;
+        text-align: center;
+        font-size: 25pt;
+        }
+  </style>
+  </head>
+  <body>
+    <form name = "myform"
+    	onSubmit = "return isValid();"
+    	action="https://php.radford.edu/~aherr4/itec325/hw01/crs-cat-resp.php"
+    	method="post">
+    <p class = "head">
+    	Course Catalog Data Entry Page
+    </p>
+    <p>
+	    Enter your first name:
+	    <input type="text" name="first" size="25" id = "first" maxlength="25" /><br />
+	    Enter your last name:
+	    <input type="text" name="last" size="25" id = "last" maxlength="25" /><br /><br />
+	    Select the department:
+	    <select name="selectdept" id = "dept">
+	      <option></option>
+		  <option>ITEC</option>
+		  <option>MATH</option>
+		  <option>STAT</option>
+		  <option>MGMT</option>
+		  <option>MKTG</option>
+        </select><br /><br />
+        Enter the course number:
+	    <input type="text" name="coursenum" id = "coursenum" size="5" maxlength="5" /><br />
+	    Enter the course title:
+	    <input type="text" name="coursetitle" id = "coursetitle" size="30" maxlength="30" /><br /><br />
+	    Enter the course description: (max 400 characters) <br /><br />
+	    <textarea name="coursedesc" id = "coursedesc" rows="8" cols="50">
+	    </textarea>
+	    <table border = "border">
+	    <tr>
+	       <td colspan = "2" align = "center">Select the major(s) that require this course</td>
+	    </tr>
+	    <tr>
+	       <td align = "center">CSAT <input type="checkbox" name="checkcsat" value="CSAT" id="csat" onClick = "checkedCS()"/><br /></td>
+	       <td align = "center">ISAS <input type="checkbox" name="checkisas" value="ISAS" id="isas" onClick = "checkedIS()"/><br /></td>
+	    </tr>
+	    <tr>
+	       <td colspan = "2" align = "center"> Areas of Concentration</td>
+	    </tr>
+	    <tr>
+	       <td align = "right"> Computer Science <input type="checkbox" name="checkcs" value="CS" id="cs" disabled/><br />
+	                           Database 		 <input type="checkbox" name="checkdb" value="DB" id="db" disabled/><br />
+	                           Software Engineering <input type="checkbox" name="checkse" value="SE" id="se" disabled/><br />
+	                           Networks 			<input type="checkbox" name="checkn" value="N" id="n" disabled/></td>
+	       <td align = "right"> Information Systems <input type="checkbox" name="checkis" value="IS" id="is" disabled/><br />
+	            				Web Development <input type="checkbox" name="checkwd" value="WD" id="wd" disabled/></td>
+	    </tr>
+	    </table>
+	    <br /><br />
+	    <input type ="submit" value = "Submit New Course Entry" />
+	    <input type = "reset"  value = "Reset Form" />
+
+    </p>
+    	</form>
+    <h2>Source Code</h2>
+    <?php
+      ini_set('date.timezone','America/New_York');
+      echo "Last modified: ", date("Y.M.d (D) H:i.", filemtime(__FILE__)), "<br />\n";
+      error_reporting (E_ALL|E_WARNING|E_PARSE);
+      show_source(__FILE__);
+    ?>
+  <body>
+<html>
---
+++ crs-cat-resp.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+  <?php
+    ini_set('display_errors','1');
+    ini_set('display_startup_errors','1');
+  ?>
+  <head>
+  <title> Course Catalogue System </title>
+  <style type = "text/css">
+    	p.head { color: black;
+      			 margin-top: 50px;
+          		 margin-left: 150px;
+          	   	 margin-right: 150px;
+          		 border-style: solid;
+          		 border-color: red;
+          		 border-width: 10px;
+          		 padding: 30px;
+          		 text-align: center;
+          		 font-size: 25pt;
+          		 }
+        p.second { color: black;
+    	         margin-top: 50px;
+        		 margin-left: 150px;
+        		 margin-right: 150px;
+        		 border-style: solid;
+        		 border-color: black;
+        		 border-width: 10px;
+        		 padding: 30px;
+        		 text-align: left;
+        		 font-size: 20pt;
+        }
+        p.third { color: black;
+	    	     margin-top: 50px;
+		         margin-left: 150px;
+		       	 margin-right: 150px;
+	     		 border-style: solid;
+	     		 border-color: black;
+	      		 border-width: 10px;
+	      		 padding: 30px;
+	      		 text-align: left;
+	      		 font-size: 20pt;
+        }
+  </style>
+  </head>
+  <body>
+  <?php
+  function isValid()
+  {
+  	$valid = true;
+  	$ret = " idid \n jj";
+  	$printReturn = "<script type = \"text/javascript\"> alert(\"$ret\");</script>";
+
+  	if(preg_match( '/^[A-Z|a-z]+-*/', $_POST["first"]) != 1)
+  	{
+
+  		$valid = false;
+  		$ret .= "First name invalid\n";
+  	}
+  	if(preg_match( '/^[A-Z|a-z]+-*/', $_POST["last"]) != 1)
+  	{
+  		$valid = false;
+  		$ret .= "Last name invalid\n";
+  	}
+  	#if(strlen($_POST["selectdept"])< 4)
+  	#{
+  	#	$valid = false;
+  	#	$ret .= "Department not selected\n";
+  	#}
+	echo $printReturn;
+  	#echo "<script>var prin ='j";
+  	#echo $printReturn;
+    #echo "';"
+  	#echo "alert(prin);";
+  	#echo "</script>";
+  }
+  isValid();
+  ?>
+  <p class = "head">
+     Course Catalog System
+  </p>
+  <p class = "second">
+     <?php
+        echo "", $_POST["selectdept"], " ";
+        echo "", $_POST["coursenum"], "\n <br />";
+        echo strtoupper($_POST["coursetitle"]);
+     ?>
+  </p>
+  <p class = "third">
+     <?php
+        print("<u>Course Description</u><br />");
+        echo "", $_POST["coursedesc"], " ";
+     ?>
+  </p>
+  <p>
+     IT Major and Areas of Concentrations that require this course <br />
+     <?php
+        if(array_key_exists("checkcsat", $_POST))
+        {
+        	print("<b><u>CSAT</u></b> - ");
+        	if(array_key_exists("checkcs", $_POST))
+        	{
+        		print("<b>Computer Science</b><br />");
+        	}
+        	if(array_key_exists("checkdb", $_POST))
+        	{
+        		print("<b>Database</b><br />");
+        	}
+        	if(array_key_exists("checkse", $_POST))
+        	{
+        		print("<b>Software Engineering</b><br />");
+        	}
+        	if(array_key_exists("checkn", $_POST))
+        	{
+        		print("<b>Networks</b><br />");
+        	}
+
+        }
+        if(array_key_exists("checkisas", $_POST))
+        {
+            print("<b><u>ISAS</u></b> - ");
+            if(array_key_exists("checkis", $_POST))
+            {
+            	print("<b>Information Science</b><br />");
+            }
+            if(array_key_exists("checkwd", $_POST))
+            {
+            	print("<b>Web Development</b><br />");
+            }
+        }
+     ?>
+  </p>
+
+  <h2>Source Code</h2>
+  <?php
+        ini_set('date.timezone','America/New_York');
+        echo "Last modified: ", date("Y.M.d (D) H:i.", filemtime(__FILE__)), "<br />\n";
+        error_reporting (E_ALL|E_WARNING|E_PARSE);
+        show_source(__FILE__);
+    ?>
+  </body>
+</html>
